69a3b42c7fb5e82a4de112963e66763226d2021d
javi <javiramos@soliss.com>
2023-03-19 10:07:38 +0100

    61.

--- 02-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 02-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -13,27 +13,56 @@ interface Product {
   img?: string;
 }
 
+export const ProductImage = ({ img = "" }) => {
+  return (
+    <img
+      className={styles.productImg}
+      src={img ? img : noImage}
+      alt="Product"
+    />
+  );
+};
+
+export const ProductTitle = ({ title }: { title: string }) => {
+  return <span className={styles.productDescription}>{title}</span>;
+};
+
+interface ProductButtonsProps {
+  counter: number;
+  increaseBy: (value: number) => void;
+  decreaseBy: (value: number) => void;
+}
+
+export const ProductButtons = ({
+  counter,
+  increaseBy,
+  decreaseBy,
+}: ProductButtonsProps) => {
+  return (
+    <div className={styles.buttonsContainer}>
+      <button className={styles.buttonMinus} onClick={() => decreaseBy(1)}>
+        -
+      </button>
+      <div className={styles.countLabel}> {counter} </div>
+      <button className={styles.buttonAdd} onClick={() => increaseBy(1)}>
+        +
+      </button>
+    </div>
+  );
+};
+
 const ProductCard = ({ product }: Props) => {
   const { counter, increaseBy, decreaseBy } = useProduct();
 
   return (
     <div className={styles.productCard}>
-      <img
-        className={styles.productImg}
-        src={product.img ? product.img : noImage}
-        alt="Coffe Mug"
+      <ProductImage img={product.img} />
+      <ProductTitle title={product.title} />
+      <ProductButtons
+        counter={counter}
+        increaseBy={increaseBy}
+        decreaseBy={decreaseBy}
       />
-      <span className={styles.productDescription}>{product.title}</span>
-
-      <div className={styles.buttonsContainer}>
-        <button className={styles.buttonMinus} onClick={() => decreaseBy(1)}>
-          -
-        </button>
-        <div className={styles.countLabel}> {counter} </div>
-        <button className={styles.buttonAdd} onClick={() => increaseBy(1)}>
-          +
-        </button>
-      </div>
     </div>
   );
 };
