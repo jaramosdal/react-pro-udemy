4d150208bc9ce3305af0c0a15b6ae40f379d4a0e
Javier Ramos Nodal <javiramos@soliss.com>
2023-04-16 21:25:08 +0200

    111

---
+++ jrn-product-card/src/components/ProductCard.tsx
@@ -0,0 +1,67 @@
+import { createContext } from "react";
+
+import styles from "../styles/styles.module.css";
+import { useProduct } from "../hooks/useProduct";
+import {
+  InitialValues,
+  Product,
+  ProductCardHandlers,
+  ProductContextProps,
+  onChangeArgs,
+} from "../interfaces/interfaces";
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export interface Props {
+  product: Product;
+  children: (args: ProductCardHandlers) => JSX.Element;
+  // children?: React.ReactElement | React.ReactElement[];
+  className?: string;
+  style?: React.CSSProperties;
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
+  initialValues?: InitialValues;
+}
+
+export const ProductCard = ({
+  product,
+  children,
+  className,
+  style,
+  onChange,
+  value,
+  initialValues,
+}: Props) => {
+  const { counter, increaseBy, maxCount, isMaxCountReached, reset } =
+    useProduct({
+      onChange,
+      product,
+      value,
+      initialValues,
+    });
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        maxCount,
+        product,
+      }}
+    >
+      <div style={style} className={`${styles.productCard} ${className}`}>
+        {children({
+          count: counter,
+          isMaxCountReached,
+          maxCount: initialValues?.maxCount,
+          product,
+
+          increaseBy,
+          reset,
+        })}
+      </div>
+    </Provider>
+  );
+};
+
+export default ProductCard;
