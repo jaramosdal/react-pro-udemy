9ead0ee103199d1bb331df69920aebdb41bd4567
Javier Ramos Nodal <javiramos@soliss.com>
2023-04-23 12:57:47 +0200

    126.

---
+++ 02-react-app/src/03-forms/pages/RegisterPage.tsx
@@ -0,0 +1,20 @@
+import "../styles/styles.css";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Repeat Password" />
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
--- 02-react-app/src/routes/Navigation.tsx
+++ 02-react-app/src/routes/Navigation.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
 import logo from "../logo.svg";
+import RegisterPage from "../03-forms/pages/RegisterPage";
 
 const Navigation = () => {
   return (
@@ -11,10 +12,10 @@ const Navigation = () => {
           <ul>
             <li>
               <NavLink
-                to="/home"
+                to="/register"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Home
+                Register Page
               </NavLink>
             </li>
             <li>
@@ -37,6 +38,7 @@ const Navigation = () => {
         </nav>
 
         <Routes>
+          <Route path="register" element={<RegisterPage />} />
           <Route path="about" element={<h1> About Page</h1>} />
           <Route path="users" element={<h1> Users Page</h1>} />
           <Route path="home" element={<h1> Home Page</h1>} />
