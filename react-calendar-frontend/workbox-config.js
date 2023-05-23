76b84138e64325091325df07ee068f63864fc915
Javier Ramos Nodal <javiramos@soliss.com>
2023-05-23 20:22:47 +0200

    202.

---
+++ react-calendar-frontend/workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  globDirectory: "build/",
+  globPatterns: ["**/*.{json,ico,html,png,txt,css,js}"],
+  swDest: "build/sw.js",
+  swSrc: "src/sw-template.js",
+  //   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/], generateSW no funciona con esta propiedad
+};
