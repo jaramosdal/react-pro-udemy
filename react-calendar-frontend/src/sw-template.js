6251ff2d3ea1cf5f433ec34ca601ed7966b019ed
Javier Ramos Nodal <javiramos@soliss.com>
2023-05-28 19:58:26 +0200

    209.

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -9,30 +9,30 @@ workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
+const cacheNetworkFirst = ["/api/auth/renew", "/api/events"];
+const cacheFirst = [
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css",
+];
 
-registerRoute(
-  new RegExp(
-    "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-  ),
-  new CacheFirst()
-);
+registerRoute(({ request, url }) => {
+  if (cacheNetworkFirst.includes(url.pathname)) {
+    return true;
+  }
+  return false;
+}, new NetworkFirst());
 
-registerRoute(
-  new RegExp(
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
-  ),
-  new CacheFirst()
-);
+registerRoute(({ request, url }) => {
+  if (cacheFirst.includes(url.href)) {
+    return true;
+  }
+  return false;
+}, new CacheFirst());
 
-registerRoute(
-  new RegExp("http://localhost:4000/api/auth/renew"),
-  new NetworkFirst()
-);
-
-registerRoute(
-  new RegExp("http://localhost:4000/api/events"),
-  new NetworkFirst()
-);
+// registerRoute(
+//   new RegExp("http://localhost:4000/api/auth/renew"),
+//   new NetworkFirst()
+// );
 
 // Posteos Offline
 const bgSyncPlugin = new BackgroundSyncPlugin("posteos-offline", {
@@ -45,4 +45,20 @@ registerRoute(
     plugins: [bgSyncPlugin],
   }),
   "POST"
+);
+
+registerRoute(
+  new RegExp("http://localhost:4000/api/events"),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "PUT"
+);
+
+registerRoute(
+  new RegExp("http://localhost:4000/api/events"),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "DELETE"
 );
